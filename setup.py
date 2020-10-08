4764678a8c4d34086cfefe7322fad85c3c751af2
Markian Hromiak <mhromiak3@gatech.edu>
2020-10-07 23:37:53 -0400

    Updated setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import setuptools
+import pathlib
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README").read_text()
 
 setuptools.setup(
     name="wasteland_sort",
-    version="0.0.1",
+    version="1.0.0",
     author="Markian Hromiak",
-    author_email="mhromiak3@gatech.edu",
-    description="Draft version of the sort with testing and comparison",
-    long_description=long_description,
+    author_email="markian.hromiak@yahoo.com",
+    description="Fully tested and documented version of the Wasteland sorting algorithm",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/MHromiak/wasteland_sort.git",
-    packages=setuptools.find_packages(),
+    url="https://github.com/MHromiak/wasteland-sort.git",
+    packages=setuptools.find_packages(exclude=("test",)),
     classifiers=[
         "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8.2',
+    python_requires='>=3.8.2',    
 )
